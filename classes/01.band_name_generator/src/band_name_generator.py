98e845869aff0ab01ea8fc96ac379f1b86c192dd
Henrique P. Rosa <henrique.prosa@hotmail.com>
2024-04-04 21:01:51 +0100

    feat: added challenge 01 files

---
+++ classes/01.band_name_generator/src/band_name_generator.py
@@ -0,0 +1,8 @@
+greeting = 'Hello! How are you?'
+
+print(greeting)
+input_city = input('Type the name of the City you grow in: \n')
+input_pet = input('Type a name of a Fluffy Pet: \n')
+
+
+print('\n\n Your Band name is: \n === THE Sweet ' + input_pet + ' Rock ' + input_city +  ' ! =====')
